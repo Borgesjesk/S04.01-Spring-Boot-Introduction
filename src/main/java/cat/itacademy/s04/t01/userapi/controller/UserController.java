58d340a74ef58847ec5a9948481966e38aaf5678
BORGES <jessborgesb@gmail.com>
2026-04-18 17:25:05 +0200

    feat: add POST /users endpoint to create new user

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -1,13 +1,13 @@
 package cat.itacademy.s04.t01.userapi.controller;
 
-
 import cat.itacademy.s04.t01.userapi.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -18,5 +18,12 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return users;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        user.setId(UUID.randomUUID());
+        users.add(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
