22fde5508ce567e12b3b5c34784b5ae6c58d5cd8
BORGES <jessborgesb@gmail.com>
2026-04-18 16:50:49 +0200

    feat: add UserController with GET /users endpoint

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -0,0 +1,22 @@
+package cat.itacademy.s04.t01.userapi.controller;
+
+
+import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final List<User> users = new ArrayList<>();
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
