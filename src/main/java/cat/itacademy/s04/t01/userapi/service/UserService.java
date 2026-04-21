fd5d514ee4d7bb04924ec935e7918daad3d02371
BORGES <jessborgesb@gmail.com>
2026-04-21 10:47:31 +0200

    feat: add UserService interface

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/service/UserService.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t01.userapi.service;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    User create(User user);
+    List<User> getAllUsers(String name);
+    User getUserById(UUID id);
+}
