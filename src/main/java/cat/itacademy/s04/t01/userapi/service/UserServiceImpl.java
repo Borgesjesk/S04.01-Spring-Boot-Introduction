7fcc40a1a9164a387d1a982f2347a8f848598ec0
BORGES <jessborgesb@gmail.com>
2026-04-23 11:49:47 +0200

    feat: add duplicate email validation in createUser(TDD green phase)

--- src/main/java/cat/itacademy/s04/t01/userapi/service/UserServiceImpl.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t01.userapi.service;
 
+import cat.itacademy.s04.t01.userapi.exception.EmailAlreadyExistsException;
 import cat.itacademy.s04.t01.userapi.exception.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.model.User;
 import cat.itacademy.s04.t01.userapi.repository.UserRepository;
@@ -19,6 +20,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(User user) {
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already exists: " + user.getEmail());
+        }
         user.setId(UUID.randomUUID());
         return repository.save(user);
     }
